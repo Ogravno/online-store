be5173674ab0c810d4f4d0512412786e9214f972
odgra003 <odgra003@osloskolen.no>
2023-03-16 13:19:28 +0100

    Initial Commit

---
+++ online-store/online_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  online_storeApp.swift
+//  online-store
+//
+//  Created by Odin Grav on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct online_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
